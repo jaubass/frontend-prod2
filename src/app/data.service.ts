29586347ed89ba8081e4ec00cd803a886405770a
jaubass <jaubass@gmail.com>
2023-10-29 14:44:28 +0100

    Estilos Pag Detalle

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  public selectedDaySubject = new Subject<number>();
+  public selectedDay: number = 0; // Agrega esta propiedad
+
+  setSelectedDay(day: number) {
+    this.selectedDay = day; // Actualiza la propiedad con el día seleccionado
+    this.selectedDaySubject.next(day);
+  }
+
+  getSelectedDay() {
+    return this.selectedDaySubject.asObservable();
+  }
+
 
   constructor(private http: HttpClient) { }
 
--- src/app/player/player/player.component.ts
+++ src/app/player/player/player.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from './../../data.service';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
+  dia: any;
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    // Accede al día seleccionado desde DataService
+    this.dia = this.dataService.selectedDay;
+  }
 }
