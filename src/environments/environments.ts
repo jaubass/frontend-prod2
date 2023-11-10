a0a321b8699d77b973616c70d6bec1a78b7dcf6d
jaubass <jaubass@gmail.com>
2023-11-10 22:31:56 +0100

    Conected to db

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: "code67-prod2",
+    appId: "1:760374712131:web:03638612b91b36f27c41f6",
+    storageBucket: "code67-prod2.appspot.com",
+    locationId: "europe-west",
+    apiKey: "AIzaSyAmguoSO3XGG2PRjb_b9_x6YrmV9HF0Qmw",
+    authDomain: "code67-prod2.firebaseapp.com",
+    messagingSenderId: "760374712131",
+  },
+  production: false
+};
