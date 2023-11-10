86bf65cf0981ec7b5db067d10eb33c2816585bf9
jaubass <jaubass@gmail.com>
2023-11-10 21:45:04 +0100

    db.ts config

---
+++ src/config/db.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAmguoSO3XGG2PRjb_b9_x6YrmV9HF0Qmw",
+  authDomain: "code67-prod2.firebaseapp.com",
+  projectId: "code67-prod2",
+  storageBucket: "code67-prod2.appspot.com",
+  messagingSenderId: "760374712131",
+  appId: "1:760374712131:web:03638612b91b36f27c41f6"
+};
