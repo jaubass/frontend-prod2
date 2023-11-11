a6294b7e53f42e3bdd0446d2850b4078b9935b80
GSpadolini <giancarlospadolini@gmail.com>
2023-11-11 15:33:43 +0100

    actualizandoDeJau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { initializeApp } from "firebase/app";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend-prod1';
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyCnToeV7j7Z8PQoJcY0NNBpG6hiB8GboIY",
+  //   authDomain: "frontend-5088a.firebaseapp.com",
+  //   projectId: "frontend-5088a",
+  //   storageBucket: "frontend-5088a.appspot.com",
+  //   messagingSenderId: "417204717894",
+  //   appId: "1:417204717894:web:2c5f8e35bdc24eb13f3d47"
+  // };
+  // const app = initializeApp(firebaseConfig);
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
    firebase: {
     apiKey: "AIzaSyCnToeV7j7Z8PQoJcY0NNBpG6hiB8GboIY",
     authDomain: "frontend-5088a.firebaseapp.com",
@@ -7,5 +6,6 @@ export const environment = {
     storageBucket: "frontend-5088a.appspot.com",
     messagingSenderId: "417204717894",
     appId: "1:417204717894:web:2c5f8e35bdc24eb13f3d47"
-   }
+   },
+   production: false
 };
