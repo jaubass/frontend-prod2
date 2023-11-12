d2dde70c07919d8f3195ecdd9a31908c3c732d82
jaubass <jaubass@gmail.com>
2023-11-12 15:13:35 +0100

    form create day

--- src/app/new-day/new-day.component.ts
+++ src/app/new-day/new-day.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-day.component.css']
 })
 export class NewDayComponent {
+  numeroDia: number;
+  ciudad: string;
+  alojamiento: string;
+  actividades: string;
+  descripcion: string;
+  videoResumen: string;
+  valoracion: number;
 
+  constructor() {
+    this.numeroDia = 0;
+    this.ciudad = '';
+    this.alojamiento = '';
+    this.actividades = '';
+    this.descripcion = '';
+    this.videoResumen = '';
+    this.valoracion = 0;
+  }
+
+  onSubmit() {
+    // Aquí puedes trabajar con los datos que se han introducido en el formulario
+    console.log('Número de Día:', this.numeroDia);
+    console.log('Ciudad:', this.ciudad);
+    console.log('Alojamiento:', this.alojamiento);
+    console.log('Actividades:', this.actividades);
+    console.log('Descripción:', this.descripcion);
+    console.log('Video Resumen:', this.videoResumen);
+    console.log('Valoración:', this.valoracion);
+
+    // Aquí podrías, por ejemplo, enviar los datos a un servicio o hacer alguna operación con ellos
+  }
 }
+
